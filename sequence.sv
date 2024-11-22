c91142a46f3f9d42a381b093f71cf753f5dc6be8
bhendi-boi <bhendiisgreen@gmail.com>
2024-11-22 14:09:30 +0530

    stopped using `uvm_do macros

--- sequence.sv
+++ sequence.sv
@@ -9,7 +9,31 @@ class reset_seq extends uvm_sequence;
 
     task body();
         tr = transaction::type_id::create("tr");
-        `uvm_do_with(tr, {wb_rst_i == 1;});
+        start_item(tr);
+        tr.randomize() with {
+            wb_rst_i == 1;
+        };
+        finish_item(tr);
+    endtask
+
+endclass
+
+class valid_rand_seq extends uvm_sequence;
+    `uvm_object_utils(valid_rand_seq)
+
+    transaction tr;
+
+    function new(string name = "valid_rand_seq");
+        super.new(name);
+    endfunction
+
+    task body();
+        tr = transaction::type_id::create("tr");
+        start_item(tr);
+        tr.randomize() with {
+            wb_rst_i == 0;
+        };
+        finish_item(tr);
     endtask
 
 endclass
