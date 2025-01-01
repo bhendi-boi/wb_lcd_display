a97a6c314217bd7f6e490db6d66cd5a350c1d43c
bhendi-boi <bhendiisgreen@gmail.com>
2025-01-01 13:31:44 +0530

    temporarily commenting `do_print`

--- seq_item.sv
+++ seq_item.sv
@@ -24,14 +24,14 @@ class transaction extends uvm_sequence_item;
     endfunction
 
 
-    function void do_print(uvm_printer printer);
-        super.do_print(printer);
-        printer.print_field_int("Reset", wb_rst_i, 1, UVM_HEX);
-        printer.print_field_int("Write/Read", wb_we_i, 1, UVM_HEX);
-        printer.print_field_int("Address", wb_adr_i, 7, UVM_HEX);
-        printer.print_field_int("Byte select", wb_sel_i, 4, UVM_HEX);
-        printer.print_field_int("Write Data", wb_dat_i, 8, UVM_HEX);
-        printer.print_field_int("Read Data", wb_dat_o, 8, UVM_HEX);
-    endfunction
+    // function void do_print(uvm_printer printer);
+    //     super.do_print(printer);
+    //     printer.print_field_int("Reset", wb_rst_i, 1, UVM_HEX);
+    //     printer.print_field_int("Write/Read", wb_we_i, 1, UVM_HEX);
+    //     printer.print_field_int("Address", wb_adr_i, 7, UVM_HEX);
+    //     printer.print_field_int("Byte select", wb_sel_i, 4, UVM_HEX);
+    //     printer.print_field_int("Write Data", wb_dat_i, 8, UVM_HEX);
+    //     printer.print_field_int("Read Data", wb_dat_o, 8, UVM_HEX);
+    // endfunction
 
 endclass
